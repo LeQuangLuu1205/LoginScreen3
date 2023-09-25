7dbb1e7bface4a7011c2c7113e780802d1a91b18
LeQuangLuu <luyenluuchimai@gmail.com>
2023-09-25 15:58:15 +0700

    Login Screen 3

---
+++ app/src/main/java/com/example/loginscreen3/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.loginscreen3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, sologan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView);
+        sologan = findViewById(R.id.textView2);
+
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        sologan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair(image, "logo_image");
+                pairs[1] = new Pair(logo, "logo_text");
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                startActivity(intent, options.toBundle());
+            }
+        },SPLASH_SCREEN);
+    }
+}
